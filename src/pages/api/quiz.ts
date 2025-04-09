a0180c38565cfb8ebe4c9a19f0152efa61dd3ec6
soyello <wodus9193@naver.com>
2025-04-10 00:01:07 +0900

    퀴즈 데이터 api 생성

---
+++ src/pages/api/quiz.ts
@@ -0,0 +1,50 @@
+import { TQuiz, TQuizResponse } from '@/types/quiz';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const questionsAndAnswers: TQuiz[] = [
+  { id: 1, question: '1+1 ?', options: ['1', '2', '3', '4'], answer: '2' },
+  { id: 2, question: '1+2 ?', options: ['1', '2', '3', '4'], answer: '3' },
+  { id: 3, question: '2+2 ?', options: ['1', '2', '3', '4'], answer: '4' },
+  { id: 4, question: '2+3 ?', options: ['5', '2', '3', '4'], answer: '5' },
+];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<TQuizResponse>) {
+  try {
+    const { page } = req.query as { page: string };
+    if (!page) {
+      res.send(questionsAndAnswers);
+    }
+    let pageToNum = parseInt(page);
+    const quiz = questionsAndAnswers[pageToNum];
+    const lastPage = questionsAndAnswers.length - 1;
+    const numOfQuestion = questionsAndAnswers.length;
+
+    if (pageToNum === 0) {
+      res.send({
+        prev: false,
+        next: true,
+        page,
+        quiz,
+        total: numOfQuestion,
+      });
+    } else if (pageToNum === lastPage) {
+      res.send({
+        prev: true,
+        next: false,
+        page,
+        quiz,
+        total: numOfQuestion,
+      });
+    } else {
+      res.send({
+        prev: true,
+        next: false,
+        page,
+        quiz,
+        total: numOfQuestion,
+      });
+    }
+  } catch (error) {
+    res.status(500);
+  }
+}
---
+++ src/types/quiz.ts
@@ -0,0 +1,16 @@
+export type TQuiz = {
+  id: number;
+  question: string;
+  options: string[];
+  answer: string;
+};
+
+export type TQuizResponse =
+  | {
+      prev: boolean;
+      next: boolean;
+      page: string;
+      quiz: TQuiz;
+      total: number;
+    }
+  | TQuiz[];
